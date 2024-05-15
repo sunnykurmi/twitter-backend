c8af00b6ad79e259ac710c472325cddfeba62c0e
sunnykurmi <sunnykurmiskks@gmail.com>
2024-05-15 23:27:14 +0530

    sa

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -31,7 +31,7 @@ router.get("/signout", isAuthenticated, usersignout);
 router.post("/newpost", isAuthenticated, newpost);
 
 //get/student/signout
-router.post("/deletepost/:id", isAuthenticated, deletepost);
+router.post("/deletepost/:id", z, deletepost);
 
 //get/student/signout
 router.post("/newcomment", isAuthenticated, newcomment);
